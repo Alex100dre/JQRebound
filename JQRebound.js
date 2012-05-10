c423403ee217248fb4f802bab2537a0f8d8c0b71
Alexandre Hachim <alex100dre.pro@gmail.com>
2012-05-10 16:51:52 +0200

    version_d'essai

--- JQRebound.js
+++ JQRebound.js
@@ -6,40 +6,41 @@
 (function($){
 
 	//attache la fonction rebound à jQuery
-	$.fn.rebound = function(options){
+	$.fn.JQRebound = function(options){
 		
 		//paramètres par défaut
 		var settings =$.extend({
-			
-			
+			//----------------------------variables
+			begin	:	0,
+			up		:	50,
+			speed	:	800,
+			run		:	true,
+			replay	:	true
 		}, options);
 		
-			//----------------------------variables
-			var up =50;
-			var speed = 800
-			var run = true;
-			var replay = true;
+		   
 			//----------------------------animation
 			function loop(){
-				if (run==true){
-					if(replay==true){
-						$(".rebound").animate({"top":"0px"},speed,'easeOutCirc').animate({top: up+'px' },speed, 'easeInCirc', loop );
-					}else{
-						$(".rebound").animate({"top":"0px"},speed,'easeOutCirc').animate({top: up+'px' },speed, 'easeOutBounce');
+				if (settings.run==true){
+					if(settings.replay==true){
+						$(".rebound").animate({"top":settings.begin+"px"},settings.speed,'easeOutCirc').animate({top: settings.up+'px' },settings.speed, 'easeInCirc', loop );
+					}else {
+						$(".rebound").animate({"top":settings.begin+"px"},settings.speed,'easeOutCirc').animate({top: settings.up+'px' },settings.speed, 'easeOutBounce');
 					}
 				}
 			};
 			//----------------------------au survole
 			$('.rebound').mouseover(function(){
-				run=true;
+				settings.run=true;
 				loop();
 			});
 			//-----------------------------en dehors
 			$('.rebound').mouseout(function(){
-				$(".rebound").stop().animate({"top":"0px"},100);
-				run=false;
+				$(".rebound").stop().animate({"top":settings.begin+"px"},100);
+				settings.run=false;
 
 			});
+		
 	}
 
 })(jQuery);
